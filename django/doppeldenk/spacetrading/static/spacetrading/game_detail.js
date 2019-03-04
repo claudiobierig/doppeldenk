c6f28f6bd767e64eb7b1acd969c4b70e85ac181a
Claudio <claudiobierig@gmail.com>
2019-03-04 20:57:16 +0100

    mark clicked hex

--- django/doppeldenk/spacetrading/static/spacetrading/game_detail.js
+++ django/doppeldenk/spacetrading/static/spacetrading/game_detail.js
@@ -1,8 +1,21 @@
 
+lastClickedHex = null
+
 function clickHex(hex_element)
 {
     document.getElementById("id_coord_q").value = hex_element.getAttribute("coord_q")
     document.getElementById("id_coord_r").value = hex_element.getAttribute("coord_r")
+
+    hex_element.style['stroke-width'] = 1.5
+    hex_element.style['stroke-opacity'] = 1
+    hex_element.style.stroke = "red"
+    if (lastClickedHex != null){
+        lastClickedHex.style.stroke = "white"
+        lastClickedHex.style['stroke-width'] = 0.5
+        lastClickedHex.style['stroke-opacity'] = 0.5
+    }
+    
+    lastClickedHex = hex_element
     clearMarketActions()
 }
 
