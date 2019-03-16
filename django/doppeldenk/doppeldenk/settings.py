61c7f6e9c516d758063a264956abd751b2dee6a7
Claudio <claudiobierig@gmail.com>
2019-03-16 20:48:00 +0100

    Make Secret Key and Debug depending on env

--- django/doppeldenk/doppeldenk/settings.py
+++ django/doppeldenk/doppeldenk/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zfe50shay=qbd6e5(d0-ia%tozbm&uk=v8vuf=16wc8z0r3ek#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'zfe50shay=qbd6e5(d0-ia%tozbm&uk=v8vuf=16wc8z0r3ek#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
