0c55cfee2dca51dfdccdf2a35d4da71004b89317
Claudio <claudiobierig@gmail.com>
2019-03-31 09:27:41 +0200

    set Form data withoyt checking

--- spacetrading/static/spacetrading/game_detail.js
+++ spacetrading/static/spacetrading/game_detail.js
@@ -43,7 +43,11 @@ function clickHex(hex_element)
 
 function changeInfluence(amount)
 {
-    console.log(0 + amount)
+    amount_influence_name = "trade_modal_influence_amount"
+    amount_influence_element = document.getElementById(amount_influence_name)
+    old_amount = parseInt(amount_influence_element.innerHTML)
+    new_amount = Math.max(old_amount + amount, 0)
+    amount_influence_element.innerHTML = new_amount
 }
 
 function change_buy_resource(resource, amount)
@@ -62,6 +66,51 @@ function change_sell_resource(resource, amount)
     old_amount = parseInt(amount_element.innerHTML)
     new_amount = Math.max(Math.min(old_amount + amount, 9), 0)
     amount_element.innerHTML = new_amount
+}
+
+function on_close_trade_modal()
+{
+    console.log("on_close")
+    for(resource = 1; resource <= 5; resource++)
+    {
+        name_buy = "trade_modal_buy_" + resource + "_amount"
+        element_buy = document.getElementById(name_buy)
+        if(element_buy != null){
+            element_buy.innerHTML = 0
+        }
+        name_sell = "trade_modal_sell_" + resource + "_amount"
+        element_sell = document.getElementById(name_sell)
+        if(element_sell != null){
+            element_sell.innerHTML = 0
+        }
+    }
+
+    name_influence = "trade_modal_influence_amount"
+    element_influence = document.getElementById(name_influence)
+    element_influence.innerHTML = 0
+
+}
+
+function on_trade()
+{
+    console.log("on_trade")
+    for(resource = 1; resource <= 5; resource++)
+    {
+        name_buy = "trade_modal_buy_" + resource + "_amount"
+        element_buy = document.getElementById(name_buy)
+        if(element_buy != null){
+            document.getElementById("id_buy_resource_" + resource).value = parseInt(element_buy.innerHTML)
+        }
+        name_sell = "trade_modal_sell_" + resource + "_amount"
+        element_sell = document.getElementById(name_sell)
+        if(element_sell != null){
+            document.getElementById("id_sell_resource_" + resource).value = parseInt(element_sell.innerHTML)
+        }
+    }
+    
+    name_influence = "trade_modal_influence_amount"
+    element_influence = document.getElementById(name_influence)
+    document.getElementById("id_buy_influence").value = parseInt(element_influence.innerHTML)
 }
 
 const active_player = getActivePlayer()
