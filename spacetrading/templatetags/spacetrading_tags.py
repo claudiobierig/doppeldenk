98442ffea5a58f61fa431c16e9c709d4456ad299
Claudio <claudiobierig@gmail.com>
2019-05-25 15:14:33 +0200

    is_active was needed by list views

--- spacetrading/templatetags/spacetrading_tags.py
+++ spacetrading/templatetags/spacetrading_tags.py
@@ -11,3 +11,11 @@ def get_class(label):
         return "table-success"
     else:
         return "table-light"
+
+@register.simple_tag
+def is_active(player, players):
+    for other_player in players:
+        if other_player.time_spent < player.time_spent or other_player.time_spent == player.time_spent and other_player.last_move > player.last_move:
+            return False
+
+    return True
